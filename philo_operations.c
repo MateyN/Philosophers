b9fea6ee7bd7882b621d1d8ae249db8070f0a327
Matey Nikolov <mateynnikolov@gmail.com>
2022-03-31 19:32:14 +0200

    optimized

--- philo_operations.c
+++ philo_operations.c
@@ -6,41 +6,51 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 16:03:17 by mnikolov          #+#    #+#             */
-/*   Updated: 2022/03/30 11:38:38 by mnikolov         ###   ########.fr       */
+/*   Updated: 2022/03/31 19:19:32 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_write(char *str, t_philo *philo)
+void	ft_eat(t_philo *philo)
 {
-	long int		time;
-
-	time = -1;
-	time = actual_time() - philo->pointer_arg->starting;
-	if (time >= 0 && time <= 2147483647 && !check_death(philo, 0))
-	{
-		printf("%ld ", time);
-		printf("Philo %d %s", philo->id_philo, str);
-	}
+	pthread_mutex_lock(philo->send);
+	printf("%ld %d is eating\n", \
+		(time_in_ms() - philo->start), philo->id_philo);
+	pthread_mutex_unlock(philo->send);
+	philo->end = time_in_ms();
+	ft_usleep(philo->time_to_eat);
+	(philo->meal_opt)++;
 }
 
-void    ft_eating(t_philo *philo)
+void	ft_sleep(t_philo *philo)
 {
-    
+	pthread_mutex_lock(philo->send);
+	printf("%ld %d is sleeping\n", \
+		(time_in_ms() - philo->start), philo->id_philo);
+	pthread_mutex_unlock(philo->send);
+	ft_usleep(philo->time_to_sleep);
 }
-void    ft_sleeping(t_philo *philo)
-{
-    pthread_mutex_lock(&philo->pointer_arg->write);
-	ft_write("is sleeping\n", philo);
-	pthread_mutex_unlock(&philo->pointer_arg->write);
-	ft_usleep(philo->pointer_arg->time_to_sleep);
-}
-void    ft_thinking(t_philo *philo)
-{
 
-}
-void    ft_dead(t_philo *philo)
+void	ft_think(t_philo *philo)
 {
-    
+	pthread_mutex_lock(philo->send);
+	printf("%ld %d is thinking\n", \
+		(time_in_ms() - philo->start), philo->id_philo);
+	pthread_mutex_unlock(philo->send);
+}
+
+void	ft_forks(t_philo *philo)
+{
+	pthread_mutex_lock(philo->send);
+	printf("%ld %d has take a fork\n", \
+		(time_in_ms() - philo->start), philo->id_philo);
+	pthread_mutex_unlock(philo->send);
+}
+
+void	ft_is_dead(t_philo *philo)
+{
+	pthread_mutex_lock(philo->send);
+	printf("\033[0;31m%ld %d died\n", \
+		(time_in_ms() - philo->start), philo->id_philo);
 }
--- philo_utils.c
+++ philo_utils.c
@@ -6,30 +6,56 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 11:26:41 by mnikolov          #+#    #+#             */
-/*   Updated: 2022/03/30 11:34:44 by mnikolov         ###   ########.fr       */
+/*   Updated: 2022/03/31 19:25:15 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long int	get_time(void)
+long int	time_in_ms(void)
 {
-	long int			time;
-	struct timeval		start;
+	struct timeval	tv;
 
-	time = 0;
-	if (gettimeofday(&start, NULL) == -1)
-		ft_exit("Gettimeofday returned -1\n");
-	time = (start.tv_sec * 1000) + (start.tv_usec / 1000);
-	return (time);
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000 + tv.tv_usec) / 1000);
 }
 
-void	ft_usleep(long int delay)
+void	ft_usleep(long time)
 {
-	long int	start_time;
+	long int	start;
 
-	start_time = 0;
-	start_time = get_time();
-	while ((get_time() - start_time) < delay)
-		usleep(delay / 10);
+	start = time_in_ms();
+	while (time_in_ms() - start < time)
+		usleep(100);
+}
+
+void	destroy_mutexes(int num_philo, \
+    pthread_mutex_t *forks, pthread_mutex_t *send)
+{
+    int i;
+
+	i = 0;
+	while (i < num_philo)
+	{
+		pthread_mutex_destroy(&(forks[i]));
+		i++;
+	}
+	pthread_mutex_destroy(send);
+}
+
+int ft_check_is_death(t_philo *philo, int num_philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_philo)
+	{
+		if (time_in_ms() - philo[i].end > philo[i].die)
+		{
+			ft_is_dead(&(philo[i]));
+			destroy_mutexes(philo->num_philo, philo->forks, philo->send);
+			return (0);
+		}
+	}
+	return (1);
 }
