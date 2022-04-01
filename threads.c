01fdcdb82e39b55062561813995fb828716f3b87
Matey Nikolov <mateynnikolov@gmail.com>
2022-04-01 16:24:30 +0200

    threads created but need more work

--- threads.c
+++ threads.c
@@ -6,10 +6,46 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 19:26:55 by mnikolov          #+#    #+#             */
-/*   Updated: 2022/03/31 19:28:26 by mnikolov         ###   ########.fr       */
+/*   Updated: 2022/04/01 16:04:59 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// creating threads && output it 
+void    ft_threads(t_philo *philo, int num_philo)
+{
+    int i;
+
+    i = 0;
+    while(i < num_philo)
+    {
+        pthread_create(&(philo[i].id_thread), NULL, &routine, &(philo[i]));
+        i = i + 2;
+    }
+    usleep(100);
+    i = 1;
+    while (i < num_philo)
+    {
+        pthread_create(&(philo[i].id_thread), NULL, &routine, &(philo[i]));
+        i = i + 2;
+    }
+}
+
+void    *routine(void *arg)
+{
+    t_philo *philo;
+
+    philo = (t_philo*)arg;
+    while(1)
+    {
+        pthread_mutex_lock(&(philo->forks[philo->id_philo - 1]));
+		ft_forks(philo);
+		pthread_mutex_lock(&(philo->forks[philo->id_philo % philo->num_philo]));
+		ft_forks(philo);
+		ft_eat(philo);
+		pthread_mutex_unlock(&(philo->forks[philo->id_philo % philo->num_philo]));
+		pthread_mutex_unlock(&(philo->forks[philo->id_philo - 1]));
+		ft_sleep(philo);
+		ft_think(philo);
+    }
+}
